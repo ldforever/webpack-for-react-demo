9bef2638f52f902709593f7da8fe6975a653789b
chennan <2829864353@qq.com>
2019-03-16 18:58:01 +0800

    build: webpack基本配置

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # webpack-for-react-demo
+
+### 项目学习笔记
+======持续更新
---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * @file 入口文件
+ * @author chennan(2829864353@qq.com)
+ */
+
+import React  from 'react';
+import ReactDOM from 'react-dom' ;
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('app')
+);
