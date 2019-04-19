9c6044287d91c52c1649c260eafa3d838eca2771
chennan <2829864353@qq.com>
2019-04-19 16:56:24 +0800

    更新readme

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -2,10 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ['./src/index.js'], // 入口文件
+    //entry: ['./src/index.js'], // 入口文件
+    entry: {
+        app: path.join(__dirname, '../src/app.js')   //  app.js作为打包入口
+    },
     output: {
-        filename: 'bundle.js', // 打包输出文件名
-        path: path.resolve(__dirname, 'dist') // 打包输出路径（必须绝对路径，否则报错）
+        //filename: 'bundle.js', // 打包输出文件名
+        filename: '[name].[hash].js',  //  name代表entry对应的名字；hash自定加入，更新内容
+        path: path.join(__dirname, '../dist'), // 打包输出路径（必须绝对路径，否则报错）
+        publicPath: './public'
     },
     module:  {
         rules:[
@@ -20,20 +25,20 @@ module.exports = {
         ]
     },
     
-    plugin: [],
+    //plugin: [],
 
-    resolve: {},
+    //resolve: {},
 
     devServer: {
-        contentBase: path.join(_dirname, 'dist'),    // 服务器资源的根目录， 不写默认bundle.js
-        compress: true,                             // 服务器资源采用gzip压缩
-        port: 8075,                                   // 端口
+        contentBase: path.join(__dirname, 'dist'),    // 服务器资源的根目录， 不写默认bundle.js
+        compress: false,                             // 服务器资源采用gzip压缩
+        port: 9000,                                   // 端口
         overlay: true,                               // 出错代码显示在html页面上
         hot: true                                    // 热加载
     },
 
     devtool: '',
 
-    mode: 'development'
+    //mode: 'production'
 }
 
